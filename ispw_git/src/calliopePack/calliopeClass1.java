87ec8b8e4ef8ad68b1c289dcd7a9db8538b48182
umbferZ <umberto.ferracci@gmail.com>
2016-01-18 17:17:43 +0100

    Creazione pacchetto calliope ed importazione di clio

---
+++ ispw_git/src/calliopePack/calliopeClass1.java
@@ -0,0 +1,13 @@
+package calliopePack;
+
+import clioPack.*;
+
+public class calliopeClass1 extends clioClass1 {
+
+	@Override
+	public void stampa(String string) {
+		// TODO Auto-generated method stub
+		super.stampa(string);
+		System.out.println("Print by calliope:\t" + string);
+	}
+}
